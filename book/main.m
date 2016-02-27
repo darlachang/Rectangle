58d17dd469f5aef92c05856ca06c9ae267bbca27
Chang Ming-Han <minghan@ming-hantekimacbook-pro.local>
2016-02-27 12:29:00 +0800

    Initial Commit

---
+++ book/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  book
+//
+//  Created by Ming-Han Chang on 2016/2/27.
+//  Copyright © 2016年 Ming-Han Chang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
